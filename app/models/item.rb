9866afb944744f027cfb5a9b024120b9b0a30efe
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-13 13:06:41 -0500

    Feat: Update associations for items and merchants and add divs to show view

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ApplicationRecord
   self.primary_key = :id
   belongs_to :merchant
   has_many :invoice_items
-  has_many :items, through: :invoice_items
+  has_many :invoices, through: :invoice_items
 end
