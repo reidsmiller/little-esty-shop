10a34b8a6ef4c0e861fd08cb2c93cb2c04d03637
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-12 20:07:54 -0500

    Test: Add more data and update models and factory attributes

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
     name { Faker::Cannabis.strain }
-    description { Faker::Fantasy::Tolkein.poem }
+    description { Faker::Quotes::Chiquito.joke }
     unit_price { rand(1000..100000) }
   end
 end
--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -9,6 +9,13 @@ require 'rails_helper'
 RSpec.describe 'Merchant Dashboard/Show Page' do
   let!(:merchant) { create(:merchant) }
   
+  let!(:item_1) { create(:item, merchant_id: merchant.id) }
+  let!(:item_2) { create(:item, merchant_id: merchant.id) }
+  let!(:item_3) { create(:item, merchant_id: merchant.id) }
+  let!(:item_4) { create(:item, merchant_id: merchant.id) }
+  let!(:item_5) { create(:item, merchant_id: merchant.id) }
+  let!(:item_6) { create(:item, merchant_id: merchant.id) }
+
   let!(:customer_1) { create(:customer, first_name: 'Branden', last_name: 'Smith') }
   let!(:customer_2) { create(:customer, first_name: 'Reilly', last_name: 'Robertson') }
   let!(:customer_3) { create(:customer, first_name: 'Grace', last_name: 'Chavez') }
@@ -23,14 +30,21 @@ RSpec.describe 'Merchant Dashboard/Show Page' do
   let!(:invoice_5) { customer_5.invoices.create }
   let!(:invoice_6) { customer_6.invoices.create }
 
-  let!(:inv_1_transaction_s) { invoice_1.transactions.create(create_list(10, result: 1)) }
-  let!(:inv_1_transaction_f) { invoice_1.transactions.create(create_list(:transaction, 5, result: 0)) }
-  let!(:inv_2_transaction_s) { invoice_2.transactions.create(create_list(:transaction, 5, result: 1)) }
-  let!(:inv_3_transaction_s) { invoice_3.transactions.create(create_list(:transaction, 7, result: 1)) }
-  let!(:inv_4_transaction_s) { invoice_4.transactions.create(create_list(:transaction, 3, result: 1)) }
-  let!(:inv_4_transaction_f) { invoice_4.transactions.create(create_list(:transaction, 20, result: 0)) }
-  let!(:inv_5_transaction_s) { invoice_5.transactions.create(create_list(:transaction, 11, result: 1)) }
-  let!(:inv_6_transaction_s) { invoice_6.transactions.create(create_list(:transaction, 8, result: 1)) }
+  let!(:invoice_item_1) { create(:invoice_item, item_id: item_1.id, invoice_id: invoice_1.id) }
+  let!(:invoice_item_2) { create(:invoice_item, item_id: item_2.id, invoice_id: invoice_2.id) }
+  let!(:invoice_item_3) { create(:invoice_item, item_id: item_3.id, invoice_id: invoice_3.id) }
+  let!(:invoice_item_4) { create(:invoice_item, item_id: item_4.id, invoice_id: invoice_4.id) }
+  let!(:invoice_item_5) { create(:invoice_item, item_id: item_5.id, invoice_id: invoice_5.id) }
+  let!(:invoice_item_6) { create(:invoice_item, item_id: item_6.id, invoice_id: invoice_6.id) }
+
+  let!(:inv_1_transaction_s) { create_list(:transaction, 10, result: 1, invoice_id: invoice_1.id) }
+  let!(:inv_1_transaction_f) { create_list(:transaction, 5, result: 0, invoice_id: invoice_1.id) }
+  let!(:inv_2_transaction_s) { create_list(:transaction, 5, result: 1, invoice_id: invoice_2.id) }
+  let!(:inv_3_transaction_s) { create_list(:transaction, 7, result: 1, invoice_id: invoice_3.id) }
+  let!(:inv_4_transaction_s) { create_list(:transaction, 3, result: 1, invoice_id: invoice_4.id) }
+  let!(:inv_4_transaction_f) { create_list(:transaction, 20, result: 0, invoice_id: invoice_4.id) }
+  let!(:inv_5_transaction_s) { create_list(:transaction, 11, result: 1, invoice_id: invoice_5.id) }
+  let!(:inv_6_transaction_s) { create_list(:transaction, 8, result: 1, invoice_id: invoice_6.id) }
   
   describe 'displays Merchants and their most recent attributes' do
     it 'should display the name of the merchant' do
@@ -44,16 +58,24 @@ RSpec.describe 'Merchant Dashboard/Show Page' do
   describe 'Top 5 Customers Column' do
     it 'should display the names of the top 5 customers by successful transactions in descending order' do
       visit "/merchants/#{merchant.id}/dashboard"
-      require 'pry'; binding.pry
+      
       within('#top_5_customers') do
         expect(customer_5.first_name).to appear_before(customer_1.first_name)
         expect(customer_1.first_name).to appear_before(customer_6.first_name)
         expect(customer_6.first_name).to appear_before(customer_3.first_name)
         expect(customer_3.first_name).to appear_before(customer_2.first_name)
+
         expect(customer_5.last_name).to appear_before(customer_1.last_name)
         expect(customer_1.last_name).to appear_before(customer_6.last_name)
         expect(customer_6.last_name).to appear_before(customer_3.last_name)
         expect(customer_3.last_name).to appear_before(customer_2.last_name)
+
+        expect(customer_5.first_name).to appear_before(customer_5.last_name)
+        expect(customer_1.first_name).to appear_before(customer_1.last_name)
+        expect(customer_6.first_name).to appear_before(customer_6.last_name)
+        expect(customer_3.first_name).to appear_before(customer_3.last_name)
+        expect(customer_2.first_name).to appear_before(customer_2.last_name)
+
         expect(page).to_not have_content(customer_4.first_name)
         expect(page).to_not have_content(customer_4.last_name)
       end
