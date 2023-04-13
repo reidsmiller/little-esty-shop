4042f8bfb78760f7faace03c66a1f35e280c5108
Reid Miller <reid.s.miller@gmail.com>
2023-04-13 11:55:20 -0600

    feat: Add validations to all models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   self.primary_key = :id
+  validates :name, presence: true
+  validates :description, presence: true
   belongs_to :merchant
   has_many :invoice_items
   has_many :items, through: :invoice_items
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,5 @@
 class Merchant < ApplicationRecord
   self.primary_key = :id
+  validates :name, presence: true
   has_many :items
 end
