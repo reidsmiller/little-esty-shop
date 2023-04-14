707b468fe30cc6dbe25d4f754543cd57c12924bc
Reid Miller <reid.s.miller@gmail.com>
2023-04-13 20:24:46 -0600

    feat: Add admin merchants grouped by status functionality

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,5 +4,13 @@ class Merchant < ApplicationRecord
   validates :status, presence: true
   has_many :items
 
-  enum status: ['enabled', 'disabled']
+  enum status: ['disabled', 'enabled']
+
+  def self.enabled?
+    where(status: :enabled)
+  end
+
+  def self.disabled?
+    where(status: :disabled)
+  end
 end
