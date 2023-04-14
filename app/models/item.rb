fcb8c37ce3eee5beb9c20bd4d9f6c4172f16381a
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-04-14 16:11:35 -0600

    Feat: Add instance method to format the the unit price into dollars

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  def format_unit_price
+    (unit_price / 100.0).round(2).to_s
+  end
 end
