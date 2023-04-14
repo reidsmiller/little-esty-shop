ae16f740a8af5aafc6eb4929414e7700f237ca51
Reid Miller <reid.s.miller@gmail.com>
2023-04-13 19:12:13 -0600

    test: Add test for admin merchant enable/disable

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -20,20 +20,21 @@ RSpec.describe 'admin_merchants_index', type: :feature do
     end
 
     it 'when I click merchant name link I am taken to that merchants admin show page and see the name of that merchant' do
-      merchant_1 = create(:merchant)
-      merchant_2 = create(:merchant)
       visit admin_merchants_path
-      expect(page).to have_link("#{merchant_1.name}")
-      expect(page).to have_link("#{merchant_2.name}")
 
-      click_link "#{merchant_1.name}"
-      expect(current_path).to eq(admin_merchant_path(merchant_1))
-      expect(page).to have_content("#{merchant_1.name}")
+      click_link "#{@merchant_1.name}"
+      expect(current_path).to eq(admin_merchant_path(@merchant_1))
+      expect(page).to have_content("#{@merchant_1.name}")
 
       visit admin_merchants_path
-      click_link "#{merchant_2.name}"
-      expect(current_path).to eq(admin_merchant_path(merchant_2))
-      expect(page).to have_content("#{merchant_2.name}")
+      click_link "#{@merchant_2.name}"
+      expect(current_path).to eq(admin_merchant_path(@merchant_2))
+      expect(page).to have_content("#{@merchant_2.name}")
+    end
+
+    it 'I see a button next to each merchant to disable or enable that merchant' do
+      visit admin_merchants_path
+      expect(page).to button()
     end
   end
 end
