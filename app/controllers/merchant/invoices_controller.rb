d32fa33409052be8d6ae5f8e24e4e24f697f6f35
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-14 00:45:46 -0500

    Feat: MVC flow for merchants/invoices index

--- app/controllers/merchant/invoices_controller.rb
+++ app/controllers/merchant/invoices_controller.rb
@@ -1,5 +1,10 @@
 class Merchant::InvoicesController < ApplicationController
   def index
+    @invoices = Invoice.all
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def show
     
   end
 end
