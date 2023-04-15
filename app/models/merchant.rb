3442b3f5d2fedd8ff061a05279039b2a884491f8
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-15 01:12:25 -0500

    Fix: Clean up code and remove prys

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,6 @@
 class Merchant < ApplicationRecord
   self.primary_key = :id
+  validates :name, presence: true
   has_many :items
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
@@ -7,15 +8,17 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   def top_five_customers
-    Customer.joins(:transactions)
-    .select("customers.*, COUNT(result)")
-    .group(:id).where("transactions.result = 1", "customers.merchants = #{@merchant}")
-    .order(count: :desc).limit(5)
+    Customer.joins(invoices: [:transactions, :invoice_items => :item])  
+    .select("customers.*, COUNT(result) as transactions_count")
+    .group(:id)
+    .where("transactions.result = ? AND items.merchant_id = ?", 1, self.id)
+    .order(transactions_count: :desc).limit(5)
   end
 
   def unshipped_items
-    Item.joins(:invoice_items)
-    .select("items.*, invoice_items.status")
-    .where("invoice_items.status != 2", "items.merchant = #{merchant}")
+    Item.joins(invoices: [:invoice_items])
+    .select("items.*, invoices.created_at as creation_date, invoice_items.status")
+    .where("invoice_items.status != 2 AND items.merchant_id = ?", self.id)
+    .order(:creation_date)
   end
 end
