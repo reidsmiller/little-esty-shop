1c2e8cfd324f5e3a2cf5fb3f064cb09019476b60
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-04-14 23:48:09 -0600

    Feat: Complete user_story_7 functionallity

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -2,4 +2,9 @@ class Merchant::ItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
+  end
 end
