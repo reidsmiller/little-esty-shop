124d4303c09444fc42cb9ee0b2b186cdbca4c9f4
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-17 22:44:18 -0500

    Feat: Add new controller for merchant::invoice_items controller and update invoices controller

--- app/controllers/merchant/invoices_controller.rb
+++ app/controllers/merchant/invoices_controller.rb
@@ -3,8 +3,9 @@ class Merchant::InvoicesController < ApplicationController
     @invoices = Invoice.all
     @merchant = Merchant.find(params[:merchant_id])
   end
-
+  
   def show
     @invoice = Invoice.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
