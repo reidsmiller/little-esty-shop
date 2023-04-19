aa27ed7f9e408cca50c592c38456e2d9090edd19
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-04-19 14:55:48 -0600

    Feat: implement likes functionality

--- app/poros/logo_image.rb
+++ app/poros/logo_image.rb
@@ -1,7 +1,8 @@
 class LogoImage
-  attr_reader :image_url
+  attr_reader :image_url, :image_likes
 
   def initialize(data)
     @image_url = data[:urls][:thumb]
+    @image_likes = data[:likes]
   end
 end
