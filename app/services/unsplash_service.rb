4513bb10b3c010ee4ee5b464261fb79d0ee6b73b
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-19 01:06:46 -0500

    Feat: Add an API image to show page

--- app/services/unsplash_service.rb
+++ app/services/unsplash_service.rb
@@ -3,4 +3,9 @@ class UnsplashService
     response = HTTParty.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
+  
+  def merchant_images
+    get_url("https://api.unsplash.com/photos/random?client_id=nJm3PMI0ChpgEr9xIkC_gqCPpvJF3J8mshaK517tRPI")
+  end
 end
+
