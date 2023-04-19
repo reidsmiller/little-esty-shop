948ee1a08841adbc6ffe98969bf537edab362323
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-19 01:08:02 -0500

    Feat: Use MVC flow to add API image to dashboard

--- app/controllers/merchant/dashboards_controller.rb
+++ app/controllers/merchant/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class Merchant::DashboardsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
+    @merchant_image = MerchantImageSearch.new.merchant_image
   end
 end
