2c5297f54c64c6805bddcf6e3e6ac9f2c8fbd3f6
Reid Miller <reid.s.miller@gmail.com>
2023-04-22 13:46:41 -0600

    test: Add test for merchant dashboard link

--- app/services/unsplash_service.rb
+++ app/services/unsplash_service.rb
@@ -9,10 +9,10 @@ class UnsplashService
   end
   
   def merchant_images
-    get_url("https://api.unsplash.com/photos/random?client_id=nJm3PMI0ChpgEr9xIkC_gqCPpvJF3J8mshaK517tRPI")
+    get_url("https://api.unsplash.com/photos/random?client_id=CMrjMuK68BSuSMfw-mJ7RHkuQz3fmkkGeay-5NbUZSk")
   end
 
   def photo(item)
-    get_url("https://api.unsplash.com/photos/random?client_id=GFgfSzqSWLE9wIIgZXasjLpoY5FFbWbbOir28ZaAaOQ&query=#{item}")
+    get_url("https://api.unsplash.com/photos/random?client_id=CMrjMuK68BSuSMfw-mJ7RHkuQz3fmkkGeay-5NbUZSk&query=#{item}")
   end
 end
